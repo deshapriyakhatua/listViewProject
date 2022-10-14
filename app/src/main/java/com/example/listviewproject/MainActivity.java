9bacd7bd73829de2864b93406855eb91f9b4a249
deshapriyakhatua <deshapriyakhatua@gmail.com>
2022-10-14 11:15:52 +0530

    14 10 2022

--- app/src/main/java/com/example/listviewproject/MainActivity.java
+++ app/src/main/java/com/example/listviewproject/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,27 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<Data> list = new ArrayList<>();
 
-        list.add(new Data("H","HackerRank","We missed you try solving","Hi Deshapriya we missed you join our..."));
-        list.add(new Data("I","InterViewBit","Welcome to interviewbit","Welcome to interviewbit ! if you have any..."));
-        list.add(new Data("C","CodeChef","We missed You","Hi Deshapriya we missed you try solving..."));
-        list.add(new Data("G","GitHUb","[GitHUb] Please verify your device","Hi Deshapriya we missed you..."));
-        list.add(new Data("L","LeetCode","We missed You","Weekly contest 315 sign in and register..."));
-        list.add(new Data("G","Google","Security alert","A new sign-in on windows deshapriya..."));
-        list.add(new Data("D","Definedge","Link Google","Hi Deshapriya link your google account..."));
-        list.add(new Data("H","HackerRank","We missed you try solving","Hi Deshapriya we missed you join our..."));
-        list.add(new Data("I","InterViewBit","Welcome to interviewbit","Welcome to interviewbit ! if you have any..."));
-        list.add(new Data("C","CodeChef","We missed You","Hi Deshapriya we missed you try solving..."));
-        list.add(new Data("G","GitHUb","[GitHUb] Please verify your device","Hi Deshapriya we missed you..."));
-        list.add(new Data("L","LeetCode","We missed You","Weekly contest 315 sign in and register..."));
-        list.add(new Data("G","Google","Security alert","A new sign-in on windows deshapriya..."));
-        list.add(new Data("D","Definedge","Link Google","Hi Deshapriya link your google account..."));
-        list.add(new Data("H","HackerRank","We missed you try solving","Hi Deshapriya we missed you join our..."));
-        list.add(new Data("I","InterViewBit","Welcome to interviewbit","Welcome to interviewbit ! if you have any..."));
-        list.add(new Data("C","CodeChef","We missed You","Hi Deshapriya we missed you try solving..."));
-        list.add(new Data("G","GitHUb","[GitHUb] Please verify your device","Hi Deshapriya we missed you..."));
-        list.add(new Data("L","LeetCode","We missed You","Weekly contest 315 sign in and register..."));
-        list.add(new Data("G","Google","Security alert","A new sign-in on windows deshapriya..."));
-        list.add(new Data("D","Definedge","Link Google","Hi Deshapriya link your google account..."));
+        list.add(new Data("H","#6F38C5","HackerRank","We missed you try solving","Hi Deshapriya we missed you join our..."));
+        list.add(new Data("I","#319DA0","InterViewBit","Welcome to interviewbit","Welcome to interviewbit ! if you have any..."));
+        list.add(new Data("C","#87A2FB","CodeChef","We missed You","Hi Deshapriya we missed you try solving..."));
+        list.add(new Data("G","#FF9F29","GitHUb","[GitHUb] Please verify your device","Hi Deshapriya please verify your device..."));
+        list.add(new Data("L","#FF8787","LeetCode","We missed You","Weekly contest 315 sign in and register..."));
+        list.add(new Data("G","#905E96","Google","Security alert","A new sign-in on windows deshapriya..."));
+        list.add(new Data("D","#FF74B1","Definedge","Link Google","Hi Deshapriya link your google account..."));
+        list.add(new Data("H","#6F38C5","HackerRank","We missed you try solving","Hi Deshapriya we missed you join our..."));
+        list.add(new Data("I","#319DA0","InterViewBit","Welcome to interviewbit","Welcome to interviewbit ! if you have any..."));
+        list.add(new Data("C","#87A2FB","CodeChef","We missed You","Hi Deshapriya we missed you try solving..."));
+        list.add(new Data("G","#FF9F29","GitHUb","[GitHUb] Please verify your device","Hi Deshapriya please verify your device..."));
+        list.add(new Data("L","#FF8787","LeetCode","We missed You","Weekly contest 315 sign in and register..."));
+        list.add(new Data("G","#905E96","Google","Security alert","A new sign-in on windows deshapriya..."));
+        list.add(new Data("D","#FF74B1","Definedge","Link Google","Hi Deshapriya link your google account..."));
+        list.add(new Data("H","#6F38C5","HackerRank","We missed you try solving","Hi Deshapriya we missed you join our..."));
+        list.add(new Data("I","#319DA0","InterViewBit","Welcome to interviewbit","Welcome to interviewbit ! if you have any..."));
+        list.add(new Data("C","#87A2FB","CodeChef","We missed You","Hi Deshapriya we missed you try solving..."));
+        list.add(new Data("G","#FF9F29","GitHUb","[GitHUb] Please verify your device","Hi Deshapriya please verify your device..."));
+        list.add(new Data("L","#FF8787","LeetCode","We missed You","Weekly contest 315 sign in and register..."));
+        list.add(new Data("G","#905E96","Google","Security alert","A new sign-in on windows deshapriya..."));
+        list.add(new Data("D","#FF74B1","Definedge","Link Google","Hi Deshapriya link your google account..."));
 
 
         CustomAdapter customAdapter = new CustomAdapter(this, R.layout.card_view, list);
@@ -61,12 +63,14 @@ public class MainActivity extends AppCompatActivity {
     public class Data{
 
         String nameChar;
+        String color;
         String name;
         String subject;
         String content;
 
-        public Data(String nameChar,String name,String subject,String content){
+        public Data(String nameChar, String color, String name, String subject, String content){
             this.nameChar=nameChar;
+            this.color=color;
             this.name=name;
             this.subject=subject;
             this.content=content;
@@ -79,6 +83,10 @@ public class MainActivity extends AppCompatActivity {
         public void setNameChar(String nameChar) {
             this.nameChar = nameChar;
         }
+
+        public String getColor() { return color; }
+
+        public void setColor(String color) { this.color = color; }
 
         public String getName() {
             return name;
@@ -130,9 +138,11 @@ public class MainActivity extends AppCompatActivity {
             TextView content = convertView.findViewById(R.id.textView4);
 
             nameChar.setText(getItem(position).getNameChar());
+            nameChar.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor(getItem(position).getColor())));
             name.setText(getItem(position).getName());
             subject.setText(getItem(position).getSubject());
             content.setText(getItem(position).getContent());
+
 
             return  convertView;
         }
